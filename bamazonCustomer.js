f22f9be442cd6fcce312d99b62a42b62bba6cb82
skinsler <simonkinsler@gmail.com>
2019-05-28 22:27:18 -0500

    added prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
-var mysql = require("mysql");
+let mysql = require("mysql");
+let inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -33,4 +34,76 @@ function afterConnection() {
         
     connection.end();
   });
+}
+
+function afterConnection() {
+  connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      displayAllProducts(res);
+      purchasePrompt(res);
+
+      
+  connection.end();
+});
+}
+
+function displayAllProducts(res) {
+  res.forEach(function (item) {
+    let idString = "ID: " + item.id.toString().padEnd(3, " ");
+    let productNameString = "Product: " + item.product_name.toString().padEnd(28, " ");
+    let categoryString = "Department: " + item.department_name.toString().padEnd(25, " ");
+    let priceString = "Price: " + item.price.toFixed(2).padEnd(12, " ");
+    let stockQuantityString = "Stock: " + item.stock_quantity.toString().padEnd(10, " ");
+
+    console.log(idString + productNameString + categoryString + priceString + stockQuantityString);
+    
+
+});   
+
+}
+
+function purchasePrompt (res) {
+  // var ids = [];
+  // res.forEach(function (item) {
+  //   ids.push(item.id);
+  // });
+
+  // console.log(ids);
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the ID of the product you would like to purchase?",
+      name: "purchaseId"
+    },
+    {
+      type: "input",
+      message: "How many would you like to purchase?",
+      name: "purchaseQuantity"
+    }
+  ])
+  .then(function(inquirerResponse) {
+    console.log(inquirerResponse.purchaseId);
+
+
+    if (inquirerResponse.purchaseQuantity < 0) {
+      console.log("Purchase quantity must be greater than zero");
+      purchasePrompt(res);
+    }
+    else if (inquirerResponse.purchaseId < 0) {
+      console.log("Purchase ID must be greater than zero");
+      purchasePrompt(res);
+
+    }
+    else {
+      order(inquirerResponse.purchaseId, inquirerResponse.purchaseQuantity);
+    }
+
+    });
+  };
+
+function order(id, quantity) {
+  console.log("ORDER!"
+   + id + quantity);
+
 }
